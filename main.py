2269bd1f94be6c35e415cba4b2ea1a6ce9a65a8e
frostfox11 <137136853+frostfox11@users.noreply.github.com>
2023-11-08 17:14:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,231 @@
+
+def jim():
+  left_or_right = input("now we got to settle if we go right left or straight what do you think\n")
+  if left_or_right == "left":
+    print("lets go\n")
+    bob()
+  elif left_or_right =="straight":
+    print("ok sounds good\n")
+    hide_or_run = input("i hear somthing bad a chill in the air i dont know what but i think we should hide\n")
+    if hide_or_run =="hide":
+      print("good i dont think it can here us we should be on our way if we still have the chance\n")
+    else:
+      print("ok lets get out of here\n")
+      jim()
+  else:
+    print("ok sounds good\n")
+    fight_or_run = input("ohhh no is that what i think it is!! it's a mummy ohh god no do you think we should fight or run?\n")
+    if fight_or_run == "fight":
+      print("YOU DIED\n")
+    else:
+      print("he is coming fast we got to run faster\n")
+      bob()
+def bob():
+  jump_or_repel = input(f"thats a pit it looks like a dead end but can almost make out somthing on the other side do we try to jump or do we repel down?\n")
+  if jump_or_repel == "jump":
+    print("you fell and DIED\n")
+  else:
+    print("good we got to the bottom it seems stable at least i dont think we could have made that jump if we tried\n")
+    jim()
+continue_or_run = input("You walk into a dangerous tomb do you leave or continue with me for wealth and fame? (Continue / Run): \n")   
+if continue_or_run == "Run":
+  print("you coward")
+  print("go outside\n")
+elif continue_or_run == "":
+  print("f**k off")
+else:
+  print("well glad we got that settled now lets continue i think it's this way\n")
+  jim()
+
+g=input()
+g=input()
+g=input()
+g=input()
+g=input()
+g=input()
+g=input()
+g=input()
+g=input()
+g=input()
+g=input()
+g=input()
+while True:
+    print('Enter your age:')
+    age = int(input())
+    if age < 1:
+        print('Please enter a positive number.')
+    break
+print(f'Your age is {age}.')
+while True:
+    print('Enter your age:')
+    age = int(input())
+    if age < 1:
+        print('Please enter a positive number.')
+    break
+print(f'Your age is {age}.')
+while True:
+    print('Enter your age:')
+    age = int(input())
+    if age < 1:
+        print('Please enter a positive number.')
+    break
+print(f'Your age is {age}.')
+while True:
+    print('Enter your age:')
+    age = int(input())
+    if age < 1:
+        print('Please enter a positive number.')
+    break
+print(f'Your age is {age}.')
+while True:
+    print('Enter your age:')
+    age = int(input())
+    if age < 1:
+        print('Please enter a positive number.')
+    break
+print(f'Your age is {age}.')
+while True:
+    print('Enter your age:')
+    age = int(input())
+    if age < 1:
+        print('Please enter a positive number.')
+    break
+print(f'Your age is {age}.')
+while True:
+    print('Enter your age:')
+    age = int(input())
+    if age < 1:
+        print('Please enter a positive number.')
+    break
+print(f'Your age is {age}.')
+while True:
+    print('Enter your age:')
+    age = int(input())
+    if age < 1:
+        print('Please enter a positive number.')
+    break
+print(f'Your age is {age}.')
+while True:
+    print('Enter your age:')
+    age = int(input())
+    if age < 1:
+        print('Please enter a positive number.')
+    break
+print(f'Your age is {age}.')
+while True:
+    print('Enter your age:')
+    age = int(input())
+    if age < 1:
+        print('Please enter a positive number.')
+    break
+print(f'Your age is {age}.')
+while True:
+    print('Enter your age:')
+    age = int(input())
+    if age < 1:
+        print('Please enter a positive number.')
+    break
+print(f'Your age is {age}.')
+while True:
+    print('Enter your age:')
+    age = int(input())
+    if age < 1:
+        print('Please enter a positive number.')
+    break
+print(f'Your age is {age}.')
+if 1 == 3:
+  print("23")
+  if 1 == 9:
+    print("12")
+  elif 1 == 4:
+    print("e4")
+  else:
+    print("3")
+if 1 == 3:
+  print("23")
+  if 1 == 9:
+    print("12")
+  elif 1 == 4:
+    print("e4")
+  else:
+    print("3")
+if 1 == 3:
+  print("23")
+  if 1 == 9:
+    print("12")
+  elif 1 == 4:
+    print("e4")
+  else:
+    print("3")
+if 1 == 3:
+  print("23")
+  if 1 == 9:
+    print("12")
+  elif 1 == 4:
+    print("e4")
+  else:
+    print("3")
+if 1 == 3:
+  print("23")
+  if 1 == 9:
+    print("12")
+  elif 1 == 4:
+    print("e4")
+  else:
+    print("3")
+if 1 == 3:
+  print("23")
+  if 1 == 9:
+    print("12")
+  elif 1 == 4:
+    print("e4")
+  else:
+    print("3")
+if 1 == 3:
+  print("23")
+  if 1 == 9:
+    print("12")
+  elif 1 == 4:
+    print("e4")
+  else:
+    print("3")
+if 1 == 3:
+  print("23")
+  if 1 == 9:
+    print("12")
+  elif 1 == 4:
+    print("e4")
+  else:
+    print("3")
+if 1 == 3:
+  print("23")
+  if 1 == 9:
+    print("12")
+  elif 1 == 4:
+    print("e4")
+  else:
+    print("3")
+if 1 == 3:
+  print("23")
+  if 1 == 9:
+    print("12")
+  elif 1 == 4:
+    print("e4")
+  else:
+    print("3")
+if 1 == 3:
+  print("23")
+  if 1 == 9:
+    print("12")
+  elif 1 == 4:
+    print("e4")
+  else:
+    print("3")
+if 1 == 3:
+  print("23")
+  if 1 == 9:
+    print("12")
+  elif 1 == 4:
+    print("e4")
+  else:
+    print("3")
---
+++ readme.md
@@ -0,0 +1,11 @@
+# NAME OF YOUR GAME
+
+## Descriptions
+
+Briefly describe the premise of your game to give potential players a sense of what it will be like to play. a dungeon explorer where you enter a tomb and fight for survival 
+
+this game will be a Choose your own Adventure style story where the story will change based  on your characters actions
+
+## Flow chart
+
+Sketch a flow chart of your game. What are the options the user can choose between, and where will those choices lead them? You might want to draw this on paper, take a picture, and upload that here.
